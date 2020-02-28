b1ab378f6ba01b0112e911555121ce28f7fb39fb
aghapygad336 <46167070+aghapygad336@users.noreply.github.com>
2020-02-28 23:47:33 +0200

    Update K-Concatenation Maximum Sum.cpp

--- K-Concatenation Maximum Sum.cpp
+++ K-Concatenation Maximum Sum.cpp
@@ -1,41 +1,49 @@
-class Solution {
-public:
-    int kConcatenationMaxSum(vector<int>& arr, int k) {
-        
-        
-        
+#include <iostream>
+#include<string.h>
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <climits>
+
+int main()
+{int k= 3;
+std::vector<int> arr = { 1, 2 };
+
 int *array;
-	array = (int*) malloc(k*sizeof(int));
        int xLen=k*arr.size();
+
+	array = (int*) malloc(xLen*sizeof(int));
       // std::cout <<"Length of the vector :" <<xLen << std::endl;
     int x=0;
       int shiftB=0;
-      
-      
+
+
 for(int y =0 ; y < k;y++){
 while(x<arr.size()){
              array[shiftB+x] = arr[x];
-        //   std::cout <<"***"<<array[shiftB+x]<< std::endl;
+         std::cout <<"***"<<arr.size()<< std::endl;
 
        x++;}
              shiftB=shiftB+arr.size();
 }
 
-           int max_so_far = INT_MIN, max_ending_here = 0; 
-  
-    for (int i = 0; i <xLen; i++) 
-    { 
-        max_ending_here = max_ending_here + array[i]; 
-        if (max_so_far < max_ending_here) 
-            max_so_far = max_ending_here; 
-  
-        if (max_ending_here < 0) 
-            max_ending_here = 0; 
-    } 
- 
-        
-        
-         return max_so_far; 
-   
-    
-}};
+           int max_so_far = INT_MIN, max_ending_here = 0;
+
+    for (int i = 0; i <xLen; i++)
+    {
+        max_ending_here = max_ending_here + array[i];
+        if (max_so_far < max_ending_here)
+            max_so_far = max_ending_here;
+
+        if (max_ending_here < 0)
+            max_ending_here = 0;
+    }
+
+
+
+
+
+
+
+    return 0;
+}
