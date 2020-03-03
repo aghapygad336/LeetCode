dcd16d5728ed892b7162611fcbc87987eeca258f
aghapygad336 <46167070+aghapygad336@users.noreply.github.com>
2020-03-03 20:27:36 +0200

    Update K-Concatenation Maximum Sum.cpp

--- K-Concatenation Maximum Sum.cpp
+++ K-Concatenation Maximum Sum.cpp
@@ -9,36 +9,48 @@ int main()
 {int k= 3;
 std::vector<int> arr = { 1, 2 };
 
+
 int *array;
+int n =arr.size();
        int xLen=k*arr.size();
 
 	array = (int*) malloc(xLen*sizeof(int));
-    int x=0;
       int shiftB=0;
 
+         
 
-for(int y =0 ; y < k;y++){
+for(int y =0 ; y < k;y++){    
+    int x=0;
 while(x<arr.size()){
              array[shiftB+x] = arr[x];
 
        x++;}
              shiftB=shiftB+arr.size();
+
+    
 }
 
-           int max_so_far = INT_MIN, max_ending_here = 0;
+
+
+
+
+        int max_so_far = INT_MIN, max_ending_here = 0;
 
     for (int i = 0; i <xLen; i++)
     {
-        max_ending_here = max_ending_here + array[i];
+        max_ending_here = max_ending_here + array[i%n];
         if (max_so_far < max_ending_here)
             max_so_far = max_ending_here;
 
         if (max_ending_here < 0)
-            max_ending_here = 0;
+            max_ending_here  = 0;
     }
-
-
-
+    if (max_so_far < 0){
+        max_so_far=0;
+    }
+        
+        
+        
 
 
 
